e5c0109b7c4b61a81f6105be552c44a4b4cd5e2f
prajolshrestha <prajolstha9@gmail.com>
2024-01-21 16:48:10 +0100

    controlflow rust

---
+++ rust/basics/control_flow/src/main.rs
@@ -0,0 +1,20 @@
+
+fn main() {
+    // Condition test must be a boolean! (can't convert from an int, etc)
+    let test = 6;
+    if test % 4 == 0 {
+        println!("Test is divisible by 4!");
+    } else if test % 3 == 0{
+        println!("Test is divisible by 3!");
+    } else if test % 2 == 0 {
+        println!("Test is divisible by 2!");
+    } else {
+        println!("Test is not divisible by 4,3,2!");
+    }
+
+    // "if" is an expression, it can be on the right side of a let statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is {}", number);
+
+}
