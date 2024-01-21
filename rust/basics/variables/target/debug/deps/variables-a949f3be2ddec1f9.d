1848f68603af1a08c47545d6ef631c8a7dcf6ed3
prajolshrestha <prajolstha9@gmail.com>
2024-01-21 16:27:13 +0100

    rust variables

---
+++ rust/basics/variables/target/debug/deps/variables-a949f3be2ddec1f9.d
@@ -0,0 +1,5 @@
+/home/codebind/programming-and-maths-Problems/rust/basics/variables/target/debug/deps/variables-a949f3be2ddec1f9: src/main.rs
+
+/home/codebind/programming-and-maths-Problems/rust/basics/variables/target/debug/deps/variables-a949f3be2ddec1f9.d: src/main.rs
+
+src/main.rs:
---
+++ rust/basics/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/codebind/programming-and-maths-Problems/rust/basics/variables/target/debug/variables: /home/codebind/programming-and-maths-Problems/rust/basics/variables/src/main.rs
