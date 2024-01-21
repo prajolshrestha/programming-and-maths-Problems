a661282b132d1650064c42d3aa99c9d8c45e7c78
prajolshrestha <prajolstha9@gmail.com>
2024-01-21 19:13:07 +0100

    Recursive cmake

---
+++ cpp_from_scratch/software_development_basics/cmake_basics/cmake5/build/math/CMakeFiles/math.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/codebind/programming-and-maths-Problems/cpp_from_scratch/software_development_basics/cmake_basics/cmake5/math/add.cpp" "math/CMakeFiles/math.dir/add.cpp.o" "gcc" "math/CMakeFiles/math.dir/add.cpp.o.d"
+  "/home/codebind/programming-and-maths-Problems/cpp_from_scratch/software_development_basics/cmake_basics/cmake5/math/multiply.cpp" "math/CMakeFiles/math.dir/multiply.cpp.o" "gcc" "math/CMakeFiles/math.dir/multiply.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp_from_scratch/software_development_basics/cmake_basics/cmake5/build/math/CMakeFiles/math.dir/multiply.cpp.o.d
@@ -0,0 +1,3 @@
+math/CMakeFiles/math.dir/multiply.cpp.o: \
+ /home/codebind/programming-and-maths-Problems/cpp_from_scratch/software_development_basics/cmake_basics/cmake5/math/multiply.cpp \
+ /usr/include/stdc-predef.h
