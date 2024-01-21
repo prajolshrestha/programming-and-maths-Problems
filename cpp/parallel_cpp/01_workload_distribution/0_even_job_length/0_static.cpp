5b9a3b2abb4010e9e41a1cd777b37502d2b6eea7
prajolshrestha <prajolstha9@gmail.com>
2024-01-21 16:52:56 +0100

    parallel_cpp

---
+++ cpp/parallel_cpp/01_workload_distribution/0_even_job_length/0_static.cpp
@@ -0,0 +1,37 @@
+#include <tbb/parallel_for.h>
+
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <iterator> 
+#include <chrono>
+
+int main() {
+
+    // Create a random number generator 
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    // Create 1 distribution
+    std::uniform_int_distribution bin(20, 30);
+
+    // Calculate the number elements per bin
+    int num_work_items = 1 << 18;
+
+    // Create work items
+    std::vector<int> work_items;
+    std::generate_n(std::back_inserter(work_items), num_work_items, 
+                [&] {return bin(mt); });
+    
+    // Process all elements in a parallel_for loop
+    tbb::parallel_for(
+        tbb::blocked_range<int>(0, num_work_items),
+        [&](tbb::blocked_range<int> r) {
+            for (int i= r.begin(); i<r.end(); i++) {
+                std::this_thread::sleep_for(std::chrono::microseconds(work_items[i]));
+            }
+        },
+        tbb::static_partitioner());
+
+    return 0;
+}
