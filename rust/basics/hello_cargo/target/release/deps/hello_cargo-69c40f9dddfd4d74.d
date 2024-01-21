f477946fe83dcbd326913e42370ddd7cd42e7cf6
prajolshrestha <prajolstha9@gmail.com>
2024-01-21 16:23:58 +0100

    cargo package manager basics

---
+++ rust/basics/hello_cargo/target/release/deps/hello_cargo-69c40f9dddfd4d74.d
@@ -0,0 +1,5 @@
+/home/codebind/programming-and-maths-Problems/rust/basics/hello_cargo/target/release/deps/hello_cargo-69c40f9dddfd4d74: src/main.rs
+
+/home/codebind/programming-and-maths-Problems/rust/basics/hello_cargo/target/release/deps/hello_cargo-69c40f9dddfd4d74.d: src/main.rs
+
+src/main.rs:
---
+++ rust/basics/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/codebind/programming-and-maths-Problems/rust/basics/hello_cargo/target/release/hello_cargo: /home/codebind/programming-and-maths-Problems/rust/basics/hello_cargo/src/main.rs
