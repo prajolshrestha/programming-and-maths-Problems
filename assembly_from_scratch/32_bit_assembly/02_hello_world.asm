b624c31a0af55bd3dac9ea147bb9501c7a213030
prajolshrestha <prajolstha9@gmail.com>
2024-02-07 15:44:53 +0100

    comments added in assembly code

--- assembly_from_scratch/32_bit_assembly/02_hello_world.asm
+++ assembly_from_scratch/32_bit_assembly/02_hello_world.asm
@@ -1,3 +1,8 @@
+;-------------------------------------------------------------------------------------------------------------------
+; Hello everyone! - (c) 2024 PRAJOL SHRESTHA - Use at your own risk, no waranty!
+;-------------------------------------------------------------------------------------------------------------------
+
+
 global _start
 
 section .data                       ; inline data into the program that we can ref. in the code by name
@@ -6,17 +11,17 @@ section .data                       ; inline data into the program that we can r
                                     ; we could inline the length as an integer which is 14 in this case
                                     ;But, doing like our code , allows us to change the string without having to count and update the length manually
 
-section .text ; This section is where our code lives.
+section .text                       ; This section is where our code lives.
 _start:
-    mov eax, 4   ;sys_write system call
-    mov ebx, 1   ;stdout file descriptor
-    mov ecx, msg ;bytes to sys_write  ; it holds string pointer
-    mov edx, len ;number of bytes to write ; it holds string length
-    int 0x80     ; perform system call ; interrupt
+    mov eax, 4                      ;sys_write system call
+    mov ebx, 1                      ;stdout file descriptor
+    mov ecx, msg                    ;bytes to sys_write                ;it holds string pointer
+    mov edx, len                    ;number of bytes to write          ;it holds string length
+    int 0x80                        ;perform system call               ;interrupt
     
-    mov eax, 1   ;sys_exit system call
-    mov ebx, 0   ;exit status is 0 ; means program run successfully
+    mov eax, 1                      ;sys_exit system call
+    mov ebx, 0                      ;exit status is 0 ; means program run successfully
     int 0x80
 
 ; vim /usr/include/asm/unistd_32.h  --> this header file provides access to various POSIX OS APIs
-; man 2 write      --> to see info about sys call
+; man 2 write                       --> to see info about sys call
